69d1e05e3e55a71df529d8771ed7934af0322acc
Hariprasath R <balaprasad24@gmail.com>
2021-11-25 13:10:33 +0530

    swagger config

---
+++ src/main/java/in/hp/java/springcloudgatewayservice/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package in.hp.java.springcloudgatewayservice.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Autowired
+    RouteDefinitionLocator locator;
+
+    @Bean
+    public List<GroupedOpenApi> apis() {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+            String name = routeDefinition.getId().replaceAll("-service", "");
+            GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+        });
+        return groups;
+    }
+}
